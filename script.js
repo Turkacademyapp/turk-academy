8d08770baf01de47cba87a7e718d720f78eec5d9
Turkacademyapp <imene0379@gmail.com>
2026-07-27 19:02:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => { 
+
 const button = document.querySelector(".start");
 
 button.addEventListener("click", () => {
 
-button.style.transform = "scale(1.03)";
+    button.style.transform = "scale(1.03)";
 
-setTimeout(() => {
+    setTimeout(() => {
 
-window.location.href = "doors.html";
+        window.location.href = "doors.html";
 
-}, 1200);
+    }, 1200);
 
 });
 
+
+// =====================
+// فتح البوابة
+// =====================
+
+const openBtn = document.querySelector(".open-btn");
+const leftDoor = document.getElementById("leftDoor");
+const rightDoor = document.getElementById("rightDoor");
+
+
+if(openBtn){
+
+    openBtn.addEventListener("click", () => {
+
+        leftDoor.style.transform = "translateX(-100%)";
+
+        rightDoor.style.transform = "translateX(100%)";
+
+    });
+
+}
+
+});
