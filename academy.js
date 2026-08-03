2a7c1acbb78730299bc8b9ee48e8bb0627f60ef4
Turkacademyapp <imene0379@gmail.com>
2026-08-03 23:32:51 +0200

    Update academy.js

--- academy.js
+++ academy.js
@@ -1,1 +1,15 @@
+const door1 = document.getElementById("door1");
 
+door1.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    door1.classList.add("open");
+
+    setTimeout(() => {
+
+        window.location.href = "a1.html";
+
+    }, 350);
+
+});
